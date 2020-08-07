dd2b9a041fe5fb1d3847c0475454688d14e3b7a0
Natalie Tapias <natalie.tapias@nordstrom.com>
2020-08-06 17:33:42 -0700

    adds a Log4j configuration file, modifies build.gradle, and updates example

---
+++ src/main/java/examples/Log4jExample.java
@@ -0,0 +1,16 @@
+package examples;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jExample {
+  private static Logger logger = LogManager.getLogger(Log4jExample.class);
+
+  public static void main(String[] args) {
+    logger.debug("log4j example: a sample debug statement");
+//    logger.info("log4j Sample info");
+//    logger.warn("log4j smaple warn");
+//    logger.error("log4j sample error");
+//    logger.fatal("log4j fatal");
+  }
+}
